8ab21d64ab6bc80e0c23dc638ad859dccd64cf11
Ilya Lavrenov <ilya.lavrenov@intel.com>
2021-07-02 14:50:51 +0300

    new tensor api

--- ngraph/core/include/ngraph/runtime/host_tensor.hpp
+++ ngraph/core/include/ngraph/runtime/host_tensor.hpp
@@ -28,87 +28,52 @@ namespace ngraph
             HostTensor(const element::Type& element_type,
                        const Shape& shape,
                        void* memory_pointer,
-                       const std::string& name = "");
+                       const Strides & strides = {});
             HostTensor(const element::Type& element_type,
-                       const Shape& shape,
-                       const std::string& name = "");
+                       const Shape& shape)
+                : HostTensor(element_type, PartialShape {shape}) {
+                create(element_type, shape);
+            }
             HostTensor(const element::Type& element_type,
-                       const PartialShape& partial_shape,
-                       const std::string& name = "");
-            HostTensor(const std::string& name = "");
-            explicit HostTensor(const Output<Node>&);
-            explicit HostTensor(const std::shared_ptr<op::v0::Constant>& constant);
+                       const PartialShape& partial_shape); // scheme
+            HostTensor();
+            // explicit HostTensor(const Output<Node>&);
+            // explicit HostTensor(const std::shared_ptr<op::v0::Constant>& constant);
             virtual ~HostTensor() override;
 
-            void initialize(const std::shared_ptr<op::v0::Constant>& constant);
+            // void initialize(const std::shared_ptr<op::v0::Constant>& constant);
 
             void* get_data_ptr();
             const void* get_data_ptr() const;
 
-            template <typename T>
-            T* get_data_ptr()
-            {
-                return static_cast<T*>(get_data_ptr());
+            void create(const element::Type& element_type, const Shape& shape) {
+                // check scheme
+                // TODO
             }
 
-            template <typename T>
-            const T* get_data_ptr() const
-            {
-                return static_cast<T*>(get_data_ptr());
+            operator void * () {
+                return nullptr;
             }
 
-            template <element::Type_t ET>
-            typename element_type_traits<ET>::value_type* get_data_ptr()
-            {
-                NGRAPH_CHECK(ET == get_element_type(),
-                             "get_data_ptr() called for incorrect element type.");
-                return static_cast<typename element_type_traits<ET>::value_type*>(get_data_ptr());
-            }
-
-            template <element::Type_t ET>
-            const typename element_type_traits<ET>::value_type* get_data_ptr() const
-            {
-                NGRAPH_CHECK(ET == get_element_type(),
-                             "get_data_ptr() called for incorrect element type.");
-                return static_cast<typename element_type_traits<ET>::value_type>(get_data_ptr());
-            }
-
-            /// \brief Write bytes directly into the tensor
-            /// \param p Pointer to source of data
-            /// \param n Number of bytes to write, must be integral number of elements.
-            void write(const void* p, size_t n) override;
-
-            /// \brief Read bytes directly from the tensor
-            /// \param p Pointer to destination for data
-            /// \param n Number of bytes to read, must be integral number of elements.
-            void read(void* p, size_t n) const override;
-
-            bool get_is_allocated() const;
-            /// \brief Set the element type. Must be compatible with the current element type.
-            /// \param element_type The element type
-            void set_element_type(const element::Type& element_type);
-            /// \brief Set the actual shape of the tensor compatibly with the partial shape.
-            /// \param shape The shape being set
-            void set_shape(const Shape& shape);
-            /// \brief Set the shape of a node from an input
-            /// \param arg The input argument
-            void set_unary(const HostTensorPtr& arg);
-            /// \brief Set the shape of the tensor using broadcast rules
-            /// \param autob The broadcast mode
-            /// \param arg0 The first argument
-            /// \param arg1 The second argument
-            void set_broadcast(const op::AutoBroadcastSpec& autob,
-                               const HostTensorPtr& arg0,
-                               const HostTensorPtr& arg1);
-            /// \brief Set the shape of the tensor using broadcast rules
-            /// \param autob The broadcast mode
-            /// \param arg0 The first argument
-            /// \param arg1 The second argument
-            /// \param element_type The output element type
-            void set_broadcast(const op::AutoBroadcastSpec& autob,
-                               const HostTensorPtr& arg0,
-                               const HostTensorPtr& arg1,
-                               const element::Type& element_type);
+            // /// \brief Set the shape of a node from an input
+            // /// \param arg The input argument
+            // void set_unary(const HostTensorPtr& arg);
+            // /// \brief Set the shape of the tensor using broadcast rules
+            // /// \param autob The broadcast mode
+            // /// \param arg0 The first argument
+            // /// \param arg1 The second argument
+            // void set_broadcast(const op::AutoBroadcastSpec& autob,
+            //                    const HostTensorPtr& arg0,
+            //                    const HostTensorPtr& arg1);
+            // /// \brief Set the shape of the tensor using broadcast rules
+            // /// \param autob The broadcast mode
+            // /// \param arg0 The first argument
+            // /// \param arg1 The second argument
+            // /// \param element_type The output element type
+            // void set_broadcast(const op::AutoBroadcastSpec& autob,
+            //                    const HostTensorPtr& arg0,
+            //                    const HostTensorPtr& arg1,
+            //                    const element::Type& element_type);
 
         private:
             void allocate_buffer();
--- ngraph/core/include/ngraph/runtime/tensor.hpp
+++ ngraph/core/include/ngraph/runtime/tensor.hpp
@@ -27,15 +27,13 @@ namespace ngraph
 
         public:
             virtual ~Tensor() {}
-            Tensor& operator=(const Tensor&) = default;
+
+            Tensor(const element::Type& element_type,
+                   const PartialShape& partial_shape); // scheme
 
             /// \brief Get tensor shape
             /// \return const reference to a Shape
             virtual const ngraph::Shape& get_shape() const;
-
-            /// \brief Get tensor partial shape
-            /// \return const reference to a PartialShape
-            const ngraph::PartialShape& get_partial_shape() const;
 
             /// \brief Get tensor element type
             /// \return element::Type
@@ -49,44 +47,15 @@ namespace ngraph
             /// \return number of bytes in tensor's allocation
             virtual size_t get_size_in_bytes() const;
 
-            /// \brief Get tensor's unique name
-            /// \return tensor's name
-            NGRAPH_DEPRECATED("Only output ports have names")
-            const std::string& get_name() const;
+            virtual create(const element::Type& element_type, const Shape& shape);
 
-            /// \brief Get the stale value of the tensor. A tensor is stale if its data is
-            /// changed.
-            /// \return true if there is new data in this tensor
-            NGRAPH_DEPRECATED("This method is deprecated and will be removed in 2022.1 release")
-            bool get_stale() const;
-
-            /// \brief Set the stale value of the tensor. A tensor is stale if its data is
-            /// changed.
-            NGRAPH_DEPRECATED("This method is deprecated and will be removed in 2022.1 release")
-            void set_stale(bool val);
-
-            /// \brief Write bytes directly into the tensor
-            /// \param p Pointer to source of data
-            /// \param n Number of bytes to write, must be integral number of elements.
-            virtual void write(const void* p, size_t n) = 0;
-
-            /// \brief Read bytes directly from the tensor
-            /// \param p Pointer to destination for data
-            /// \param n Number of bytes to read, must be integral number of elements.
-            virtual void read(void* p, size_t n) const = 0;
-
-            /// \brief check tensor for new data, call may block.
-            ///    backends may use this to ensure tensor is updated (eg: lazy eval).
-            NGRAPH_DEPRECATED("This method is deprecated and will be removed in 2022.1 release")
-            virtual void wait_for_read_ready() {}
-            /// \brief notify tensor of new data, call may block.
-            ///    backends may use this as indication of new data in tensor.
-            NGRAPH_DEPRECATED("This method is deprecated and will be removed in 2022.1 release")
-            virtual void wait_for_write_ready() {}
+            // map methods
+            void * rmap();
+            void * wmap();
+            void * rwmap();
 
         protected:
             std::shared_ptr<ngraph::descriptor::Tensor> m_descriptor;
-            bool m_stale;
         };
     } // namespace runtime
 } // namespace ngraph
